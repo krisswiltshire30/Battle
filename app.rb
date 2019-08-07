8f7175e19c5200f98f7c716ff369de6a5214c4d3
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-08-07 08:39:10 +0100

    Can input player names

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
-
 require 'sinatra/base'
 # homepage
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    # @name = params[:name]
+    erb :index
+  end
+
+  post '/names' do
+    @player_one_name = params[:player_one_name]
+    @player_two_name = params[:player_two_name]
+    erb(:play)
+  end
+
+  get '/test' do
+    'Testing123'
   end
   # start the server if ruby file executed directly
-   run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
  end
